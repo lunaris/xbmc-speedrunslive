ccb983c52942d7e78c26dd7a4aa4e100d2cd7435
Will Jones <will@sacharissa.co.uk>
2014-02-22 14:02:20 +0000

    Work in Twitch video resolution.

--- addon.py
+++ addon.py
@@ -1,13 +1,26 @@
-from xbmcswift2 import Plugin
+from xbmcswift2 import Plugin, xbmc
 
-from speedrunslive import JSONClient, SpeedRunsLiveClient, formattedItems
+from speedrunslive import WebClient, JSONClient, TwitchVideoResolver, SpeedRunsLiveClient
 
-API_ENDPOINT = 'http://api.speedrunslive.com:81'
+TWITCH_API_ENDPOINTS = {
+    'justin': 'http://www.justin.tv',
+    'twitch': 'http://www.twitch.tv',
+    'twitchApi': 'http://api.twitch.tv/api',
+    'twitchUsher': 'http://usher.twitch.tv'
+}
+
+SRL_API_ENDPOINT = 'http://api.speedrunslive.com:81'
 
 plugin = Plugin()
 logger = plugin.log
-jsonClient = JSONClient(API_ENDPOINT, logger)
-srlClient = SpeedRunsLiveClient(jsonClient, logger)
+
+webClient = WebClient(logger)
+jsonClient = JSONClient(webClient, logger)
+
+srlVideoResolver = TwitchVideoResolver(
+    TWITCH_API_ENDPOINTS, webClient, jsonClient, logger)
+
+srlClient = SpeedRunsLiveClient(SRL_API_ENDPOINT, jsonClient, logger)
 
 @plugin.route('/')
 def index():
@@ -26,6 +39,25 @@ def get_streams():
     items = formattedItems(streams)
 
     return items
+
+def formattedItems(streams):
+    for stream in streams:
+        item = {
+            'label': stream['display_name'],
+            'path': plugin.url_for('get_stream', channel=stream['name'])
+        }
+
+        yield item
+
+@plugin.route('/stream/<channel>')
+def get_stream(channel):
+    playlistFilename = (xbmc.translatePath('special://temp/') +
+        'twitchplaylist.m3u8')
+
+    srlVideoResolver.resolveChannelToPlaylist(channel, playlistFilename)
+    xbmc.Player().play(playlistFilename)
+
+    plugin.set_resolved_url(playlistFilename)
 
 if __name__ == '__main__':
     plugin.run()
--- speedrunslive.py
+++ speedrunslive.py
@@ -6,15 +6,14 @@ try:
 except:
   import simplejson as json
 
-class JSONClient(object):
-    def __init__(self, endpoint, logger):
+class WebClient(object):
+    def __init__(self, logger):
         object.__init__(self)
 
-        self.endpoint = endpoint
         self.logger = logger
 
-    def _get(self, path, headers=None):
-        request = urllib2.Request(self.endpoint + '/' + path)
+    def get(self, url, headers=None):
+        request = urllib2.Request(url)
 
         response = urllib2.urlopen(request)
         data = response.read()
@@ -22,9 +21,16 @@ class JSONClient(object):
 
         return data
 
-    def get(self, path, headers=None):
+class JSONClient(object):
+    def __init__(self, webClient, logger):
+        object.__init__(self)
+
+        self.webClient = webClient
+        self.logger = logger
+
+    def get(self, url, headers=None):
         try:
-            response = self._get(path, headers)
+            response = self.webClient.get(url, headers)
         except:
             raise Exception('HTTP error')
 
@@ -35,13 +41,83 @@ class JSONClient(object):
         except:
             raise Exception('JSON parsing error')
 
-class SpeedRunsLiveClient(object):
-    def __init__(self, jsonClient, logger):
+class TwitchVideoResolver(object):
+    _USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0'
+
+    def __init__(self, apiEndpoints, webClient, jsonClient, logger):
+        self.apiEndpoints = apiEndpoints
+
+        self.webClient = webClient
         self.jsonClient = jsonClient
         self.logger = logger
 
+    def _resolveSWFURL(self, channel):
+        swfURL = (self.apiEndpoints['justin'] +
+            '/widgets/live_embed_player.swf?channel=' + channel)
+
+        twitchURL = self.apiEndpoints['twitch'] + '/' + channel
+
+        headers = {
+            'User-Agent': _USER_AGENT,
+            'Referer': twitchURL
+        }
+
+        request = urllib2.Request(swfURL, None, headers)
+        response = urllib2.urlopen(request)
+
+        return response.geturl()
+
+    def _getToken(self, channel):
+        url = (self.apiEndpoints['twitchApi'] + '/channels/' +
+            channel + '/access_token')
+
+        obj = self.jsonClient.get(url)
+        return obj['token'], obj['sig']
+
+    def _parseUnrestrictedPlayListItems(self, data):
+        if 'No Results' not in data:
+            items = data.split('\n')
+            i = 0
+            while i < len(items):
+                item = items[i]
+                if ('EXT-X-TWITCH-RESTRICTED' not in item):
+                    yield item
+
+                i += 1
+
+        else:
+            raise Exception('Stream offline')
+
+    def _getPlaylistItems(self, channel, token, signature):
+        template = (self.apiEndpoints['twitchUsher'] +
+            '/select/{0}.m3u8?nauthsig={1}&nauth={2}&allow_source=true')
+
+        url = template.format(channel, signature, token)
+        data = self.webClient.get(url)
+
+        return self._parseUnrestrictedPlayListItems(data)
+
+    def resolveChannelToPlaylist(self, channel, playlistFilename):
+      token, signature = self._getToken(channel)
+      items = self._getPlaylistItems(channel, token, signature)
+
+      playlistFile = open(playlistFilename, 'w')
+      for item in items:
+        playlistFile.write(item + '\n')
+
+      playlistFile.close()
+
+class SpeedRunsLiveClient(object):
+    def __init__(self, apiEndpoint, jsonClient, logger):
+        self.apiEndpoint = apiEndpoint
+        self.jsonClient = jsonClient
+        self.logger = logger
+
+    def _getJSON(self, path):
+        return self.jsonClient.get(self.apiEndpoint + '/' + path)
+
     def getStreams(self):
-        obj = self.jsonClient.get('test/teams')
+        obj = self._getJSON('test/teams')
         streams = obj['channels']
 
         return self.formattedStreams(self.filteredStreams(streams))
@@ -53,11 +129,3 @@ class SpeedRunsLiveClient(object):
     def formattedStreams(self, streams):
         for stream in streams:
             yield stream['channel']
-
-def formattedItems(streams):
-    for stream in streams:
-        item = {
-            'label': stream['display_name']
-        }
-
-        yield item
